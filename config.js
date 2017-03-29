f8197769fe87e43bd5e5f73cc0c94b27400db092
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-03-29 16:15:41 +0800

    Fetch weather information from openweathermap

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ let config = {
   },
   skyscanner: {
     apikey: 'Hk616851957153764762501950591392'
+  },
+  openweathermap: {
+    apikey: '16ae93bda54235e1e098548700194d2e'
   }
 };
 
--- main.js
+++ main.js
@@ -1,67 +1,71 @@
 let config = require('./config.js');
 
-let skyscanner = require('skyscannerjs');
-
 let Telegram = require('node-telegram-bot-api');
+let request = require('request');
+let fs = require('fs');
+
+function get_reply(chatid, message) {
+  fs.writeFile('test.txt', message, e => {if (e) console.log(e)});
+  return get_weather('Tokyo');
+}
+
+function get_weather(location) {
+  return new Promise(resolve => {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?appid=' + config.openweathermap.apikey + '&q=' + location;
+    request(url, (err, res, body) => {
+      let ret = JSON.parse(body);
+      let msg = `The weather of ${ret.name} now is ${ret.weather[0].main}\nTemperature: ${Math.round((ret.main.temp - 273) * 10) / 10}°C`;
+      resolve(msg);
+    });
+  });
+}
 
 function telegram_init() {
   let telegramBot = new Telegram(config.telegram.token, {
     polling: true
   });
 
-  telegramBot.sendMessage(config.telegram.chatid, 'Telegram notification start. ', {
-    reply_markup: JSON.stringify({
-      'keyboard': [
-        [
-          {
-            text: 'Set my location',
-            request_location: true
-          },
-          {
-            text: '/myid'
-          }
-        ]
-      ],
-      'one_time_keyboard': true,
-      'force_reply': true
-  })});
+  telegramBot.sendMessage(config.telegram.chatid, 'CX chatbot start. ',
+  {
+    // reply_markup: JSON.stringify({
+    //   'keyboard': [
+    //     [
+    //       {
+    //         text: 'Set my location',
+    //         request_location: true
+    //       },
+    //       {
+    //         text: '/myid'
+    //       }
+    //     ]
+    //   ],
+    //   'one_time_keyboard': true,
+    //   'force_reply': true
+    // })
+  });
 
   telegramBot.onText(/\/myid(.*)/, function(msg) {
     telegramBot.sendMessage(msg.from.id, 'Your chat id is: ' + msg.from.id);
   });
+
+  telegramBot.onText(/.*/, function(msg) {
+    get_reply(msg.from.id, msg.text)
+      .then(message => telegramBot.sendMessage(msg.from.id, message))
+  });
+
   // telegramBot.onText(/\/start(.*)/, function(msg) {
   //   if (msg.from.id.toString() !== notificationProviders.telegram.chatid) {
   //     telegramBot.sendMessage(msg.from.id, 'Not registered chatid. ');
   //   } else {
   //   }
   // });
-  telegramBot.on('location', (msg) => {
-    var loc = msg.location;
-    console.log(loc);
-  });
+
+  // telegramBot.on('location', (msg) => {
+  //   var loc = msg.location;
+  //   console.log(loc);
+  // });
 
   console.log('Start listening ...');
 }
-
-let test_skyscanner = () => {
-  const api = new skyscanner.API(config.skyscanner.apikey);
-  console.log(api);
-
-  api.flights.livePrices.session({
-    country: "UK",
-    currency: "GBP",
-    locale: "en-GB",
-    locationSchema: "Iata",
-    originplace: "EDI",
-    destinationplace: "LHR",
-    outbounddate: "2016-06-13",
-    adults: 1
-  }).then((response) => {
-      // URL to poll the session.
-      const location = response.headers.location;
-  }).catch(console.error);
-}
-
-// test_skyscanner();
 
 telegram_init();
